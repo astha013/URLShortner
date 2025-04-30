6f1f20bb6eb310e19c10bf8721c0153c29f17b34
astha013 <asthapawar0138@gmail.com>
2025-04-30 23:46:43 +0530

    Cleaned connect.js and prepared for Render deployment

--- controllers/url.js
+++ controllers/url.js
@@ -4,7 +4,7 @@ const URL = require('../models/url')
 async function handleGenerateNewShortURL(req , res){
     const shortID = nanoid(8);
     const body = req.body;
-    // console.log(shortID);
+    console.log(shortID);
 
     if(!body.url) return  res.status(400).json({error : 'url is required'});
 
@@ -16,7 +16,7 @@ async function handleGenerateNewShortURL(req , res){
    });
 
     return res.render('home',{id : shortID})
-    return res.json({id : shortID})
+    //return res.json({id : shortID})
 }
 
 async function handleGetAnalytics(req , res){
